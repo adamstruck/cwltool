f39f3dde7a76601a86d85026f64ac61fcfbbd08f
adamstruck <strucka@ohsu.edu>
2017-05-24 09:22:13 -0700

    freeze cwltool version

--- cwltool-tes/setup.py
+++ cwltool-tes/setup.py
@@ -9,7 +9,7 @@ README = os.path.join(SETUP_DIR, 'README.md')
 
 setup(
     name='cwltool-tes',
-    version='1.0',
+    version='0.1',
     description='Common workflow language reference implementation backended \
     by a GA4GH Task Execution Service',
     long_description=open(README).read(),
@@ -18,7 +18,9 @@ setup(
     url="https://github.com/common-workflow-language/cwltool-tes",
     download_url="https://github.com/common-workflow-language/cwltool-tes",
     license='Apache 2.0',
-    install_requires=['cwltool'],
+    install_requires=[
+        'cwltool==1.0.20170510165748'
+    ],
     entry_points={
         'console_scripts': ["cwltool-tes=cwltool_tes.main:main"]},
     zip_safe=True
